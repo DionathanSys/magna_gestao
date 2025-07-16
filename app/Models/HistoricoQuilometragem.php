e3b5dfdbbf1383606ae21986d7f563b9712edad4
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-07-16 18:00:38 -0300

    fix

--- app/Models/HistoricoQuilometragem.php
+++ app/Models/HistoricoQuilometragem.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class HistoricoQuilometragem extends Model
 {
     protected $table = 'historico_quilometragens';
+
+    public function veiculo()
+    {
+        return $this->belongsTo(Veiculo::class, 'veiculo_id');
+    }
 }
