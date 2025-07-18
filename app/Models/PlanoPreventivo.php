3a9bd7418532096a66b19abd7da6b7217dd2d874
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-07-18 11:43:42 -0300

    fix

--- app/Models/PlanoPreventivo.php
+++ app/Models/PlanoPreventivo.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class PlanoPreventivo extends Model
 {
     protected $table = 'planos_preventivo';
+
+    protected $casts = [
+        'itens' => 'array',
+    ];
 }
