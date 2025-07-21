d0b794784682ebfdb964fd77ee52cd58c61b68b7
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-07-21 04:21:12 -0300

    fix

--- app/Filament/Resources/ViagemResource.php
+++ app/Filament/Resources/ViagemResource.php
@@ -415,12 +415,12 @@ class ViagemResource extends Resource
                         $record->updated_by = Auth::user()->id;
                         $record->checked_by = Auth::user()->id;
                         $record->save();
-
-                        if($record->km_cobrar > 0) {
-                            notify::success('Viagem registrada p/ cobrança.');
-                            RegistrarViagemComplementoJob::dispatch($record->id);
-                            Log::debug('Disparado job de registro de complemento da viagem: ' . $record->id);
-                        }
+                        // if($record->km_cobrar > 0) {
+                        //     ds($record->km_cobrar);
+                        //     notify::success('Viagem registrada p/ cobrança.');
+                        //     RegistrarViagemComplementoJob::dispatch($record->id);
+                        //     Log::debug('Disparado job de registro de complemento da viagem: ' . $record->id);
+                        // }
                     }),
                 Tables\Actions\Action::make('nao-conferido')
                     ->label('Ñ Conferido')
