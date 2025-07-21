f59e33991e40de919428267fc8f20bce645e05f1
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-07-21 11:47:03 -0300

    fox

--- app/Filament/Resources/VeiculoResource/RelationManagers/PneusRelationManager.php
+++ app/Filament/Resources/VeiculoResource/RelationManagers/PneusRelationManager.php
@@ -79,9 +79,9 @@ class PneusRelationManager extends RelationManager
                 Tables\Columns\TextColumn::make('km_inicial')
                     ->width('1%')
                     ->numeric(0, ',', '.'),
-                Tables\Columns\TextColumn::make('veiculo.kmAtual.quilometragem')
-                    ->width('1%')
-                    ->numeric(0, ',', '.'),
+                // Tables\Columns\TextColumn::make('veiculo.kmAtual.quilometragem')
+                //     ->width('1%')
+                //     ->numeric(0, ',', '.'),
                 Tables\Columns\TextColumn::make('data_inicial')
                     ->width('1%')
                     ->date('d/m/Y'),
