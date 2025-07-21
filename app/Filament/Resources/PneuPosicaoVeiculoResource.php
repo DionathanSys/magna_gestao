20bb1f5df40bfdbb4a45425893323416242316ab
DionathanSys <150610527+DionathanSys@users.noreply.github.com>
2025-07-21 01:57:14 -0300

    fix

--- app/Filament/Resources/PneuPosicaoVeiculoResource.php
+++ app/Filament/Resources/PneuPosicaoVeiculoResource.php
@@ -50,6 +50,7 @@ class PneuPosicaoVeiculoResource extends Resource
                     ->sortable(),
                 Tables\Columns\TextColumn::make('km_inicial')
                     ->width('1%')
+                    ->tooltip(fn (PneuPosicaoVeiculo $record): string => $record->km_inicial ? (($record->veiculo->kmAtual ?? 0) - $record->km_inicial) : 'N/A')
                     ->numeric(0, ',', '.'),
                 Tables\Columns\TextColumn::make('veiculo.kmAtual.quilometragem')
                     ->width('1%')
