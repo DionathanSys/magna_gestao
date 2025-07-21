69f6299058d3804eb81acf249a3c3954778f0f13
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-07-21 17:18:54 -0300

    fox

--- app/Filament/Resources/ViagemComplementoResource.php
+++ app/Filament/Resources/ViagemComplementoResource.php
@@ -132,6 +132,7 @@ class ViagemComplementoResource extends Resource
                                 $record->save();
                             });
                         })
+                        ->deselectRecordsAfterCompletion()
                         ->requiresConfirmation(),
             ]);
     }
