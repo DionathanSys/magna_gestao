0746cf0fb4cea873e81bf3367edbc6769881e9fa
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-07-23 17:15:10 -0300

    fox

--- app/Filament/Resources/ViagemResource/Pages/ListViagems.php
+++ app/Filament/Resources/ViagemResource/Pages/ListViagems.php
@@ -19,7 +19,7 @@ class ListViagems extends ListRecords
             Actions\ActionGroup::make([
                 Actions\Action::make('importar-viagens')
 
-                    ->successNotification(null)
+                    // ->successNotification(null)
                     ->label('Importar Viagens')
                     ->icon('heroicon-o-arrow-up-on-square')
                     ->url(route('import.viagens'))
