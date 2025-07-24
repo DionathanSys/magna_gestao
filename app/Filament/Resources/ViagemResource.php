cd1a60a11bd493f3b33b63eb45d616e7dbebd926
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-07-24 07:12:05 -0300

    fox

--- app/Filament/Resources/ViagemResource.php
+++ app/Filament/Resources/ViagemResource.php
@@ -182,7 +182,7 @@ class ViagemResource extends Resource
                                                         \Filament\Infolists\Components\TextEntry::make('numero_viagem')
                                                             ->label('Nº Viagem')
                                                             ->getStateUsing(function($record) {
-                                                                return $record->numero_viagem ?? 'N/A';
+                                                                return $ultimasCargas['viagem']['numero_viagem'] ?? 'N/A';
                                                             }),
                                                         \Filament\Infolists\Components\TextEntry::make('data_competencia')
                                                             ->label('Data Competência')
