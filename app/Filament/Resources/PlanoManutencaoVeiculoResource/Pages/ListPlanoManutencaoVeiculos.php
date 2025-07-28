c6179004af2a2e934a599b5def8b40e869004655
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-07-28 01:46:38 -0300

    fix

--- app/Filament/Resources/PlanoManutencaoVeiculoResource/Pages/ListPlanoManutencaoVeiculos.php
+++ app/Filament/Resources/PlanoManutencaoVeiculoResource/Pages/ListPlanoManutencaoVeiculos.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\PlanoManutencaoVeiculoResource\Pages;
 use App\Filament\Resources\PlanoManutencaoVeiculoResource;
 use App\Services\PlanoManutencao\PlanoManutencaoService;
 use Filament\Actions;
+use Filament\Forms;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPlanoManutencaoVeiculos extends ListRecords
@@ -18,14 +19,16 @@ class ListPlanoManutencaoVeiculos extends ListRecords
             Actions\Action::make('Gerar PDF')
                 ->label('Gerar PDF')
                 ->icon('heroicon-o-document-text')
-                ->form([
-                    \Filament\Forms\Components\TextInput::make('km_tolerancia')
-                        ->label('KM Tolerância')
-                        ->default(2500)
-                        ->numeric()
-                        ->columnSpan(1)
-                        ->required(),
-                ])
+                ->form(fn(Forms\Form $form) => $form
+                    ->columns(3)
+                    ->schema([
+                        Forms\Components\TextInput::make('km_tolerancia')
+                            ->label('KM Tolerância')
+                            ->columnSpan(1)
+                            ->default(2500)
+                            ->numeric()
+                            ->required(),
+                    ]))
                 ->action(fn($data) => (new PlanoManutencaoService)->gerarRelatorioVencimentoPdf($data['km_tolerancia']))
                 ->color('primary'),
         ];
