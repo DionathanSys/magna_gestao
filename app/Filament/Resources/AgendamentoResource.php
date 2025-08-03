e9c4af13e8ecadb023a45455917a1cfdec561d9f
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-08-03 07:45:33 -0300

    fix

--- app/Filament/Resources/AgendamentoResource.php
+++ app/Filament/Resources/AgendamentoResource.php
@@ -325,7 +325,7 @@ class AgendamentoResource extends Resource
                             $service = new AgendamentoService();
                             $service->encerrar($record);
                             if ($service->hasError()) {
-                                notify::error(mensagem: 'Agendamento: ' . $record->id . '. \n' . $service->getMessage());
+                                notify::error(mensagem: 'Agendamento: ' . $record->id . PHP_EOL . $service->getMessage());
                                 return;
                             }
                             notify::success(mensagem: $service->getMessage());
