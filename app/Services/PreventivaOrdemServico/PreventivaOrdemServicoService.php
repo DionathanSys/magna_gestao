657e526b49215204566ceccb49a7a14026926c80
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-08-03 13:02:26 -0300

    fix

--- app/Services/PreventivaOrdemServico/PreventivaOrdemServicoService.php
+++ app/Services/PreventivaOrdemServico/PreventivaOrdemServicoService.php
@@ -33,11 +33,18 @@ class PreventivaOrdemServicoService
 
             $itensPlano = $preventivaOrdemServico->planoPreventivo->itens;
 
+            Log::debug(__METHOD__ . '-' . __LINE__, [
+                'itens_plano' => $itensPlano,
+                'ordem_servico_id' => $preventivaOrdemServico->ordem_servico_id,
+            ]);
+
             foreach ($itensPlano as $item) {
+                
                 Log::debug(__METHOD__ . '-' . __LINE__, [
                     'item' => $item,
                     'ordem_servico_id' => $preventivaOrdemServico->ordem_servico_id,
                 ]);
+
                 $this->itemOrdemServicoService->create([
                     'plano_preventivo_id'   => $preventivaOrdemServico->plano_preventivo_id,
                     'ordem_servico_id'      => $preventivaOrdemServico->ordem_servico_id,
