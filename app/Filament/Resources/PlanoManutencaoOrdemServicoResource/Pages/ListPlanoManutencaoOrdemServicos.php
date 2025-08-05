158f0710316d825cd6899fd43fb5dbca770d782c
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-08-05 12:04:57 -0300

    fix

--- app/Filament/Resources/PlanoManutencaoOrdemServicoResource/Pages/ListPlanoManutencaoOrdemServicos.php
+++ app/Filament/Resources/PlanoManutencaoOrdemServicoResource/Pages/ListPlanoManutencaoOrdemServicos.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\PlanoManutencaoOrdemServicoResource\Pages;
 
 use App\Filament\Resources\PlanoManutencaoOrdemServicoResource;
 use Filament\Actions;
+use Filament\Forms;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPlanoManutencaoOrdemServicos extends ListRecords
@@ -14,7 +15,20 @@ class ListPlanoManutencaoOrdemServicos extends ListRecords
     {
         return [
             Actions\CreateAction::make()
-                ,
+                ->extraModalFooterActions(fn(Actions\Action $action): array => [
+                    $action->makeModalSubmitAction('manterVeiculo', arguments: ['another' => true]),
+                ])
+                ->action(function (Actions\Action $action, Forms\Form $form, array $data, array $arguments): void {
+                    // Create
+
+                    if ($arguments['another'] ?? false) {
+                            $form->fill([
+                                'veiculo_id' => $data['veiculo_id'],
+
+                            ]);
+                            $action->halt();
+                        }
+                }),
         ];
     }
 }
