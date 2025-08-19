7edcbda823b2c6f7c233192954181a2fad940fea
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-08-19 07:20:15 -0300

    fix

--- app/Filament/Indicadores/Resources/GestorResource/RelationManagers/ResultadosRelationManager.php
+++ app/Filament/Indicadores/Resources/GestorResource/RelationManagers/ResultadosRelationManager.php
@@ -40,7 +40,7 @@ class ResultadosRelationManager extends RelationManager
                     ->sortable(),
                 Tables\Columns\TextColumn::make('indicador.peso_por_periodo')
                     ->label('Peso Indicador')
-                    ->formatStateUsing(fn(Forms\Get $get, $state) => number_format(($get('pontuacao_obtida') / $state) * 100, 2, ',', '.'))
+                    ->formatState(fn(Forms\Get $get, $state) => number_format(($get('pontuacao_obtida') / $state) * 100, 2, ',', '.'))
                     ->prefix('%')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('status')
